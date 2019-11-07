e94ced8b6d5c76692ee5d4e5b4c07b9a8cda63b0
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-11-06 23:40:24 -0500

    add mapping component and include fetch requests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({text}) => (
+	<div
+		style={{
+			color: 'white',
+			background: 'grey',
+			padding: '15px 10px',
+			display: 'inline-flex',
+			textAlign: 'center',
+			alignItems: 'center',
+			justifyContent: 'center',
+			borderRadius: '100%',
+			transform: 'translate(-50%, -50%)'
+		}}>
+		{text}
+	</div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [vehicleActivity, setVehicleActivity] = useState([]);
+
+	const defaultProps = {
+		center: {
+			lat: 40.71641,
+			lng: -73.84363
+		},
+		zoom: 13
+	};
+
+	const fetchMtaData = async () => {
+		const response = await fetch(
+			`/api/siri/vehicle-monitoring.json?key=${process.env.REACT_APP_MTA_API_KEY}&LineRef=MTABC_QM11`
+		);
+
+		const json = await response.json();
+
+		const {
+			Siri: {
+				ServiceDelivery: {VehicleMonitoringDelivery}
+			}
+		} = json;
+		const vehicleActivity = VehicleMonitoringDelivery[0].VehicleActivity;
+		console.log(vehicleActivity);
+		setVehicleActivity(vehicleActivity);
+	};
+
+	useEffect(() => {
+		fetchMtaData();
+	}, []);
+
+	return (
+		// Important! Always set the container height explicitly
+		<div style={{height: '100vh', width: '100%'}}>
+			<GoogleMapReact
+				bootstrapURLKeys={{key: `${process.env.REACT_APP_GOOGLE_API_KEY}`}}
+				defaultCenter={defaultProps.center}
+				defaultZoom={defaultProps.zoom}>
+				{vehicleActivity &&
+					vehicleActivity.map(({MonitoredVehicleJourney: {VehicleLocation: {Latitude, Longitude}}}, idx) => (
+						<AnyReactComponent key={idx} lat={Latitude} lng={Longitude} text='My Marker' />
+					))}
+			</GoogleMapReact>
+		</div>
+	);
 }
 
 export default App;

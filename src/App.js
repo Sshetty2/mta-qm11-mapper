d11e47622e74af61a1c688e184b93add670c48b1
Sachit Shetty <sachit.shetty@bgcpartners.com>
2019-11-07 21:25:34 -0500

    update styling and add useCallback

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({text}) => (
+const Marker = ({text}) => (
 	<div
 		style={{
 			color: 'white',
-			background: 'grey',
-			padding: '15px 10px',
+			background: '#e26d40',
+			padding: '5px 5px',
 			display: 'inline-flex',
 			textAlign: 'center',
 			alignItems: 'center',
 			justifyContent: 'center',
 			borderRadius: '100%',
-			transform: 'translate(-50%, -50%)'
+			transform: 'translate(-50%, -80%)',
+			WebkitBoxShadow:"0px 10px 13px -7px #000000, inset -23px -16px 4px -6px rgba(0,0,0,0)",
+			boxShadow:"0px 10px 13px -7px #000000, inset -23px -16px 4px -6px rgba(0,0,0,0)"
 		}}>
 		{text}
 	</div>
@@ -29,9 +31,14 @@ function App() {
 		zoom: 13
 	};
 
+	const line = 'Q60';
+	const direction = '1';
+	//seconds
+	const refreshInterval = 1.5;
+
 	const fetchMtaData = async () => {
 		const response = await fetch(
-			`/api/siri/vehicle-monitoring.json?key=${process.env.REACT_APP_MTA_API_KEY}&LineRef=MTABC_QM11`
+			`/api/siri/vehicle-monitoring.json?key=${process.env.REACT_APP_MTA_API_KEY}&LineRef=MTABC_Q60&DirectionRef=${direction}`
 		);
 
 		const json = await response.json();
@@ -42,14 +49,31 @@ function App() {
 			}
 		} = json;
 		const vehicleActivity = VehicleMonitoringDelivery[0].VehicleActivity;
-		console.log(vehicleActivity);
+		console.log({vehicleActivity});
 		setVehicleActivity(vehicleActivity);
 	};
 
 	useEffect(() => {
 		fetchMtaData();
+		setInterval(() => {
+			fetchMtaData();
+		}, refreshInterval * 1000);
 	}, []);
 
+	const renderMarkers = useCallback(
+		() =>
+			vehicleActivity.map(
+				(
+					{
+						MonitoredVehicleJourney: {
+							VehicleLocation: {Latitude: latitude, Longitude: longitude}
+						}
+					},
+					idx
+				) => <Marker key={idx} lat={latitude} lng={longitude} text={line} />
+			),
+		[vehicleActivity, line]
+	);
 	return (
 		// Important! Always set the container height explicitly
 		<div style={{height: '100vh', width: '100%'}}>
@@ -57,10 +81,7 @@ function App() {
 				bootstrapURLKeys={{key: `${process.env.REACT_APP_GOOGLE_API_KEY}`}}
 				defaultCenter={defaultProps.center}
 				defaultZoom={defaultProps.zoom}>
-				{vehicleActivity &&
-					vehicleActivity.map(({MonitoredVehicleJourney: {VehicleLocation: {Latitude, Longitude}}}, idx) => (
-						<AnyReactComponent key={idx} lat={Latitude} lng={Longitude} text='My Marker' />
-					))}
+				{vehicleActivity && renderMarkers()}
 			</GoogleMapReact>
 		</div>
 	);
